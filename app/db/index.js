9d7fba0cc88474b063aa8f35234b46e6de050042
mateuszwende <matwende@gmail.com>
2019-06-21 15:50:55 +0200

    Correct the db setup

--- app/db/index.js
+++ app/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const uri = process.env === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/bestbefore';
+const uri = process.env.NODE_ENV === 'production'
+  ? process.env.MONGODB_URI
+  : 'mongodb://127.0.0.1:27017/bestbefore';
 
 module.exports = {
   connect: () => mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }),
